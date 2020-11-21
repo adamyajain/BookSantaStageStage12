84b0237610b74b700f669596e4e009bc9bf8fbf7
adamyajain <apoorvadoc@gmail.com>
2020-11-21 17:55:45 +0530

    commited

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -3,7 +3,6 @@ import { StyleSheet, View, FlatList,Text } from 'react-native';
 import { ListItem, Icon } from 'react-native-elements';
 import firebase from 'firebase';
 import MyHeader from '../components/MyHeader';
-import SwipeableFlatList from '../components/SwipableFlatList';
 
 import db from '../config';
 
